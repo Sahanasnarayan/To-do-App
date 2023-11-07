050afd2f4d0755baa3515496c00a9059c868479c
Sahana S <sahana.s@jalantechnologies.com>
2023-11-07 12:49:08 +0530

    to-doapp-folder is created

---
+++ to-doapp/src/components/signuppage/signup.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './signup.css';
+const Signup = () => {
+  return (
+    <div className="signup-container">
+      <form className="signup-form">
+        <h2>Sign Up</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input type="password" id="confirm-password" name="confirm-password" required />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
---
+++ to-doapp/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginPage from './components/loginpage/login';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LoginPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();
+
+
